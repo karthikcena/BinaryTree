be6d150063934921e0146256fce7433a9228e40e
Karthik08cse23 <56184455+karthikcena@users.noreply.github.com>
2022-01-30 14:08:56 +0800

    Added Unit Test

---
+++ Unit Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unit Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DK COMPUTRONIX")]
+[assembly: AssemblyProduct("Unit Test")]
+[assembly: AssemblyCopyright("Copyright © DK COMPUTRONIX 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5734a194-03ad-4595-bfab-85661cf209a8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Unit Test/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static BinaryTreeEval.Program;
+
+namespace Unit_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateValue()
+        {
+            string s = "((15/(7-(1+1)))*3)-(2+(1+1))";
+            s = "(" + s;
+            s += ")";
+            Node root = buildTree(s);
+            var result = evalTree(root);
+            Assert.AreEqual(result, 5);
+        }
+
+        [TestMethod]
+        public void CalculateDifferentValue()
+        {
+            string s = "(15+(6+8))";
+            s = "(" + s;
+            s += ")";
+            Node root = buildTree(s);
+            var result = evalTree(root);
+            Assert.AreEqual(result, 29);
+        }
+
+        [TestMethod]
+        public void CalculateAnotherValue()
+        {
+            string s = "((3/(7-6))*(8+(3-1)))";
+            s = "(" + s;
+            s += ")";
+            Node root = buildTree(s);
+            var result = evalTree(root);
+            Assert.AreEqual(result, 30);
+        }
+
+        [TestMethod]
+        public void HandlingNegativeInteger()
+        {
+            string s = "((15/(7-(1+1)))*-3)-(2+(1+1))";
+            s = "(" + s;
+            s += ")";
+            Node root = buildTree(s);
+            var result = evalTree(root);
+            Assert.AreEqual(result, -13);
+        }
+    }
+}
